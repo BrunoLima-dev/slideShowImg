c309e15bd172e4cb516a851a6647ddf993d448da
BrunoLima-dev <brunoprograme04@gmail.com>
2022-02-07 20:59:29 -0300

    finalização do mini projeto slide

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 
 const images = [
     { 'id': '1', 'url':'./img/chrono.jpg' },
+    { 'id': '2', 'url':'./img/inuyasha.jpg' },
+    { 'id': '3', 'url':'./img/ippo.png' },
+    { 'id': '4', 'url':'./img/tenchi.jpg' },
+    { 'id': '5', 'url':'./img/tenjhotenge.jpg' },
+    { 'id': '6', 'url':'./img/yuyuhakusho.jpg' },
     
 ]
 
@@ -17,5 +22,20 @@ const loadImages = (images, container) => {
     })
 }
 
-loadImages( images, containerItems )
+loadImages( images, containerItems );
 
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
