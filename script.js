418d9f2dce865f8fb2a9c220272da5daf5de45d9
BrunoLima-dev <brunoprograme04@gmail.com>
2022-02-08 16:23:45 -0300

    movendo de lugar a pasta img e alerando o local

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-'use strict';
+"use strict";
 
 const images = [
-    { 'id': '1', 'url':'./img/chrono.jpg' },
-    { 'id': '2', 'url':'./img/inuyasha.jpg' },
-    { 'id': '3', 'url':'./img/ippo.png' },
-    { 'id': '4', 'url':'./img/tenchi.jpg' },
-    { 'id': '5', 'url':'./img/tenjhotenge.jpg' },
-    { 'id': '6', 'url':'./img/yuyuhakusho.jpg' },
-    
-]
+  { id: "1", url: "./public/img/chrono.jpg" },
+  { id: "2", url: "./public/img/inuyasha.jpg" },
+  { id: "3", url: "./public/img/ippo.png" },
+  { id: "4", url: "./public/img/tenchi.jpg" },
+  { id: "5", url: "./public/img/tenjhotenge.jpg" },
+  { id: "6", url: "./public/img/yuyuhakusho.jpg" },
+];
 
-const containerItems = document.querySelector('#container-items');
+const containerItems = document.querySelector("#container-items");
 
 const loadImages = (images, container) => {
-    images.forEach (image => {
-        container.innerHTML += `
+  images.forEach((image) => {
+    container.innerHTML += `
             <div class='item'>
                 <img src='${image.url}'
             </div>
-        `
-    })
-}
+        `;
+  });
+};
 
-loadImages( images, containerItems );
+loadImages(images, containerItems);
 
-let items = document.querySelectorAll('.item');
+let items = document.querySelectorAll(".item");
 
 const previous = () => {
-    containerItems.appendChild(items[0]);
-    items = document.querySelectorAll('.item');
-}
+  containerItems.appendChild(items[0]);
+  items = document.querySelectorAll(".item");
+};
 
 const next = () => {
-    const lastItem = items[items.length - 1];
-    containerItems.insertBefore(lastItem, items[0]);
-    items = document.querySelectorAll('.item');
-}
+  const lastItem = items[items.length - 1];
+  containerItems.insertBefore(lastItem, items[0]);
+  items = document.querySelectorAll(".item");
+};
 
-document.querySelector('#previous').addEventListener('click', previous);
-document.querySelector('#next').addEventListener('click', next);
+document.querySelector("#previous").addEventListener("click", previous);
+document.querySelector("#next").addEventListener("click", next);
